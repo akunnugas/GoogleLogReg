ce170b954987aa8a2ceedfd2385d7f3cd19af0d3
dimas551 <ahmaddimas5151@gmail.com>
2024-10-13 10:29:01 +0700

    GoogleLogReg

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Auth\SocialiteController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/auth/redirect', [SocialiteController::class, 'redirect']);
+Route::get('/auth/callback/google', [SocialiteController::class, 'callback']);
+
+require __DIR__ . '/auth.php';
